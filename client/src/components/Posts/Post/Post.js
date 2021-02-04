c0e0a02f80e0cd02394bb3909ffdcf91dc133e88
Susanna Capacchione <57346974+Sue-svbd@users.noreply.github.com>
2021-02-04 12:06:59 +0100

    initial commit

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import useStyles from "./styles.js";
+
+const Post = () => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  console.log(posts);
+
+  return <h1>Post</h1>;
+};
+
+export default Post;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import posts from "./posts";
+
+export default combineReducers({
+  // here we can use all the reducers we have
+  posts,
+});
