bf365c41534ec457a02f6e07bb184ef633e52ec2
Susanna Capacchione <57346974+Sue-svbd@users.noreply.github.com>
2021-02-25 20:49:03 +0100

    actions name converted into costants, environmental variables created

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
 import * as api from "../api";
 // Action creators, functions that return actions
 
@@ -5,7 +6,7 @@ export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
     dispatch({
-      type: "FETCH_ALL",
+      type: FETCH_ALL,
       payload: data,
     });
   } catch (error) {
@@ -17,7 +18,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
     dispatch({
-      type: "CREATE",
+      type: CREATE,
       payload: data,
     });
   } catch (error) {
@@ -29,7 +30,7 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
 
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -39,7 +40,7 @@ export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
 
-    dispatch({ type: "DELETE", payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
@@ -49,7 +50,7 @@ export const likePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
 
-    dispatch({ type: "LIKE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error);
   }
--- server/index.js
+++ server/index.js
@@ -3,15 +3,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config(0);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-"mongodb+srv://Sue:REPREHENSIONS@cluster0.mnjnp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 const PORT = process.env.PORT || 5000;
 
